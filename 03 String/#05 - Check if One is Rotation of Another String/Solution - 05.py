69ec3c902466533f2b499aca06f1c83997b25890
Anisha Katiyar <84247530+ani14kay@users.noreply.github.com>
2022-07-05 22:03:48 +0530

    Create Solution - 05.py

---
+++ 03 String/#05 - Check if One is Rotation of Another String/Solution - 05.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        #code here
+        if len(s1)!=len(s2):
+            return 0
+        for i in range(len(s2)):
+            temp=s2[0]
+            s2=s2[1:]
+            s2+=temp
+            if s2==s1:
+                return 1
+        return 0
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s1=str(input())
+        s2=str(input())
+        if(Solution().areRotations(s1,s2)):
+            print(1)
+        else:
+            print(0)
+    
+        
+# } Driver Code Ends
