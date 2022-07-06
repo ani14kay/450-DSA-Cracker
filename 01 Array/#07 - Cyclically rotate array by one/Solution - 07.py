d9a6d598641958b160c8f5e1635e87edb317602c
Anisha Katiyar <84247530+ani14kay@users.noreply.github.com>
2022-07-06 22:33:47 +0530

    Create Solution - 07.py

---
+++ 01 Array/#07 - Cyclically rotate array by one/Solution - 07.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+def rotate( arr, n):
+    x=arr.pop()
+    arr.insert(0,x)
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        rotate(a, n)
+        print(*a)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
